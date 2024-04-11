821c296b6c18f46d76d11bbb4a71fc5eeede6f5b
Lance Anderson <lance.a.585@gmail.com>
2024-04-10 21:21:17 -0700

    initial page & loader

--- src/app/components/Initial.tsx
+++ src/app/components/Initial.tsx
@@ -1,5 +1,144 @@
+"use client";
 import React from "react";
+import { AiFillPlayCircle } from "react-icons/ai";
+import { SiEthereum } from "react-icons/si";
+import { BsInfoCircle } from "react-icons/bs";
+
+import { Loader } from "./";
+
+type InputProps = {
+  placeholder: string;
+  name: string;
+  type: string;
+  value: number;
+  handleChange: (e: React.ChangeEvent<HTMLInputElement>, name: string) => void;
+};
+
+const Input = ({
+  placeholder,
+  name,
+  type,
+  value,
+  handleChange,
+}: InputProps) => (
+  <input
+    placeholder={placeholder}
+    type={type}
+    step="0.0001"
+    value={value}
+    onChange={(e) => handleChange(e, name)}
+    className=" my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm glass-white"
+  ></input>
+);
+
+const sixSectionStyles =
+  "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
 export default function Initial() {
-  return <h1>Welcome</h1>;
+  const connectWallet = () => {};
+
+  const handleSubmit = () => {};
+
+  return (
+    <div className=" flex w-full justify-center items-center">
+      <div className=" flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+        <div className=" flex flex-1 justify-start flex-col md:mr-10">
+          <h1 className="text-3xl sm:text-5xl text-white text-unique-gradient py-1">
+            Send Ethereum <br /> around the globe
+          </h1>
+          <p className=" text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
+            Send ETH from your MetaMask wallet with Rootdown Crypto
+          </p>
+          <button
+            type="button"
+            onClick={connectWallet}
+            className=" flex flex-row justify-center items-center my-5 bg-[#2952e3] rounded-full cursor-pointer hover:bg-[#2546bd] "
+          >
+            <p className=" text-white text-base font-semibold">
+              Connect Wallet
+            </p>
+          </button>
+          <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
+            <div className={`rounded-tl-2xl ${sixSectionStyles}`}>
+              Reliability
+            </div>
+            <div className={`${sixSectionStyles}`}>Security</div>
+            <div className={`sm:rounded-tr-2xl ${sixSectionStyles}`}>
+              Ethereum
+            </div>
+            <div className={`sm:rounded-bl-2xl ${sixSectionStyles}`}>
+              Web 3.0
+            </div>
+            <div className={` ${sixSectionStyles}`}>Low Fees</div>
+            <div className={`rounded-br-2xl ${sixSectionStyles}`}>
+              Blockchain
+            </div>
+          </div>
+        </div>
+        {/* second col */}
+        <div className=" flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10">
+          {/* ethereum card */}
+          <div className=" p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 crypto-card glass-white ">
+            <div className=" flex justify-between flex-col w-full h-full">
+              <div className=" flex justify-between items-start">
+                <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
+                  <SiEthereum fontSize={21} color="fff" />
+                </div>
+                <BsInfoCircle fontSize={17} color="fff" />
+              </div>
+              <div>
+                <p className=" text-white font-light text-sm">Address</p>
+                <p className=" text-white font-semibold text-lg mt-1">
+                  Ethereum
+                </p>
+              </div>
+            </div>
+          </div>
+          {/* transaction form */}
+          <div className=" p-5 sm:w-96 w-full flex flex-col justify-start items-center glass-blue">
+            {/* inputs */}
+            <Input
+              placeholder="Address To"
+              name="addressTo"
+              type="text"
+              handleChange={() => {}}
+            />
+            <Input
+              placeholder="Amount (ETH)"
+              name="amount"
+              type="number"
+              handleChange={() => {}}
+            />
+            <Input
+              placeholder="Keyword (Gif)"
+              name="keyword"
+              type="text"
+              handleChange={() => {}}
+            />
+            <Input
+              placeholder="Enter Message"
+              name="message"
+              type="text"
+              handleChange={() => {}}
+            />
+            {/* line */}
+            <div className=" h-[1px] w-full bg-gray-400 my-2" />
+            {/* show loader if loading */}
+            {false ? (
+              <Loader />
+            ) : (
+              <button
+                type="button"
+                onClick={handleSubmit}
+                className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] rounded-full cursor-pointer"
+              >
+                Send Now
+              </button>
+            )}
+          </div>
+          {/* end 2nd column */}
+        </div>
+      </div>
+    </div>
+  );
 }
