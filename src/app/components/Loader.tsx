e04663865a80a60abad4d4c0ecdd97059efc6e22
Lance Anderson <lance.a.585@gmail.com>
2024-06-21 10:06:25 -0700

    style: change color of loader

--- src/app/components/Loader.tsx
+++ src/app/components/Loader.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Loader() {
   return (
     <div className="flex justify-center items-center py-3">
-      <div className=" animate-spin rounded-full h-32 w-32 border-b-2 border-[#986dd4]" />
+      <div className=" animate-spin rounded-full h-32 w-32 border-b-2 border-[#00C8F3]" />
     </div>
   );
 }
